b0f56e029ae0aae59ea3861fe30ba38455073ec9
atlant04 <maxatlant17@gmail.com>
2019-11-05 22:51:40 -0500

    added crns

--- utils.js
+++ utils.js
@@ -6,12 +6,18 @@ function compileMessage(message) {
     return twiml.toString()
 }
 
-function generateMessage(courses) {
+function generateMessage(courses, crn) {
     var message = ''
     for(course of courses) {
         if(course.attributes.schedule_type == "Lecture*") {
             message += '--------------------------\n'
             message += course.name + " has " + course.seats.regular + " seats remaining \n"
+            if(crn) {
+                message += "Time: " + course.attributes["time"] + "\n"
+                message += "Days: " + course.attributes["days"] + "\n"
+                message += "Professor: " + course.attributes["professor"] + "\n"
+                message += "Location: " + course.attributes["location"] + "\n"
+            }
         }
     }
     return message
